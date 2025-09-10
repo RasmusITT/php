c3ddad1af351fc3679400bfff861e494f6c4fe72
RasmusITT <rasmus.ojala1@gmail.com>
2025-09-10 11:37:36 +0300

    Add files via upload

--- h01.php
+++ h01.php
@@ -7,7 +7,123 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
   </head>
   <body>
+    <h1>Harjutus 07</h1>
+    <?php
+    function tervita(){
+        echo "Tere paikesekesekene";
+    }
+
+    function uudiskiri(){
+echo '<div class="row">
+    <div class="col-sm-2">
+       <form action="">
+        <input type="text" placeholder="Liitu uudiskirjaga">
+       <input type="submit" value"Liitu!" class="btn btn-success">
+       </form>
+   </div>
+</div>;'
+}
+
+function createUser($u){
+  $lu = strtolower($u);
+  echo $lu."@hkhk.edu.ee";
+  $p  = substr(password_hash($lu, PASSWORD_BCRYPT),7,7);
+}
+
+function vahemikus($a1, $a2, $s){
+    for ($i=$a1; $i < $a2; $i=$i+$s) {
+        echo $i;
+    } 
+}
+
+function rectangleS($a1, $a2){
+    return $a1 * $a2;
+}
+
+
+
+echo "<br>";
+
+tervita();
+uudiskiri();
+createUser("Rasmus");
+echo "<br>";
+vahemikus(2,20,3);
+echo ik("38237523089");
+echo "Pindala ".rectangleS(10,30);
+echo "<br>";
+?>
+
+
+<form>
+Kylg 1<input type="number" name="kylg1" value="10">
+Kylg 2<input type="number" name="kylg2" value="10">
+<input type="submit" value="Arvuta pindala">
+</form>
+<?php
+echo "Pindala ".rectangleS($_GET['kylg1'],$_GET['kylg2']);
+echo "<br>";
+?>
+
+<?php
+function ik($ik){
+    $pikkus = strlen($ik);
+    if ($pikkus==11) {
+        if (intval($ik[0])%2==0) {
+            $vastus="Mees";
+        } else {
+            $vastus="Naine";
+        }
+    } else {
+        $vastus="Vale pikkusega";
+    }
+    return $vastus;
+}
+echo "<br>"
+
+function headMotted(){
+    $alused = array("Jüri","Mari","Uku");
+    $oeldised = array("armastab","viskab","tõmbab");
+    $sihtitised = array("sind","mind","teda");
+
+    echo $alused[array_rand($alused)]." ".$oeldised[array_rand($oeldised)]." ".$sihtitised[array_rand($sihtitised)];
+}
+headMotted();
+
+tervita();
+uudiskiri();
+createUser("Rasmus");
+echo "<br>";
+vahemikus(2,20,3);
+echo ik("38237523089");
+echo "Pindala ".rectangleS(10,30);
+echo "<br>";
+?>
+
     <h1>Harjutus 06</h1>
+    <?php
+    $poiss = array("markus","robin","kert")
+    $tydruk = array("liisu","liisi","merlin")
+
+for ($i=0; $i < count($tydruk); $i++) {
+    echo $tydruk[$i]." - ".$p[$i]."<br>";
+}
+
+    ?>
+
+
+    <?php
+    $t = array("juuli","maali","kati");
+    $p = array("ago","marko","mati");
+
+    for ($i=0; $i < count($t); $i++) {
+        echo $t[$i]." - ".$p[$i]."<br>";
+    }
+
+    echo "<br>";
+    ?>
+
+
     <?php
     for ($i=1; $i <= 100 ; $i++) {
         if ($i%3==0)
