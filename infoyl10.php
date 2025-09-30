ad6eab5baf4aa14342f227a49976de433fb282ab
RasmusITT <rasmus.ojala1@gmail.com>
2025-09-30 11:40:33 +0300

    Delete infoyl10.php

--- infoyl10.php
+++
@@ -1,1 +0,0 @@
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, voluptatum beatae! Placeat sit nulla repudiandae repellat rem, provident id est ipsa. Repellat tempora praesentium quam porro consequatur explicabo deserunt provident.</p>
